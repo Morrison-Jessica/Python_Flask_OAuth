ac68530ff5d6a2b0920d30249fa8c09faf2b51ea
Jessica Morrison <jlmorrison@student.fullsail.edu>
2026-05-19 19:14:20 -0600

    Debug - updated with misssing libraries. OAuth with Auth0 in place, .env updated

--- server/main.py
+++ server/main.py
@@ -1,14 +1,40 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, session, request 
 from flask import jsonify
+import json 
 from datetime import timedelta
-import json
-from os import environ as env
 from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv, find_dotenv
-from functtools import wraps
+from functools import wraps
+import os
 
 
 app = Flask(__name__)
+
+oauth = OAuth(app)
+app.secret_key = os.environ.get("AUTH0_SECRET")
+app.debug = True
+AUTH0_BASE_URL= os.environ.get("AUTH0_DOMAIN")
+
+auth0 = oauth.register(
+    "auth0",
+    auth_name = "request",
+    client_id = os.environ.get("AUTH0_CLIENT_ID"),
+    client_secret = os.environ.get("AUTH0_CLIENT_SECRET"),
+    api_base_url = "http://localhost:5000/",
+    access_token_url = "",
+    authorize_url = "",
+    client_kwargs = {"scope": "openid profile email" },
+    server_metadata_url = ""
+)
+
+app.config["SESSION_TYPE"] = "filesystem"
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
 
 # ======================================== 
 # ============== Route Func ================== 
